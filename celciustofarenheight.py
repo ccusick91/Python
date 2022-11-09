7b3f97008f6b79cc1ab1f0b93130ec4d9830074b
ccusick91 <93087460+ccusick91@users.noreply.github.com>
2022-11-09 16:50:20 -0600

    Add files via upload

---
+++ celciustofarenheight.py
@@ -0,0 +1,12 @@
+print("\n\t\tHello, This app converts Celcius to Farenheight")
+
+# Equation for conversion is F=C*9/5+32
+# f=farenheight
+# c=celcius
+
+input1 = int(input("\n\t\tPlease enter degrees Celcius: "))
+
+degreesFarenheight = input1 * 9/5 + 32
+
+print("\n\t\tDegrees farenheight: {} degrees".format(degreesFarenheight))
+

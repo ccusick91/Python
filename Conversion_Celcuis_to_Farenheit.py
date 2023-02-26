4b304c3edb5ad3acd3407f5d3b869758e2172274
Cory <93087460+ccusick91@users.noreply.github.com>
2023-02-26 11:57:56 -0600

    Update and rename celciustofarenheight.py to Conversion_Celcuis_to_Farenheit.py

---
+++ Conversion_Celcuis_to_Farenheit.py
@@ -0,0 +1,12 @@
+print("\n\t\tHello, This app converts Celcius to Farenheit")
+
+# Equation for conversion is F=C*9/5+32
+# f=farenheit
+# c=celcius
+
+input1 = int(input("\n\t\tPlease enter degrees Celcius: "))
+
+degreesFarenheit = input1 * 9/5 + 32
+
+print("\n\t\tDegrees farenheit: {} degrees".format(degreesFarenheit))
+
